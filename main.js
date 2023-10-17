427d98d6a26c5a5435422e6cbf80e4fc414b9a28
esdb1 <pierre.lacognata@mail-esd.com>
2023-10-17 14:14:58 +0200

    done

--- main.js
+++ main.js
@@ -1,49 +1,120 @@
-const cercle = document.querySelector('.cercle')
-const timer = document.querySelector('.time')
-cercle.addEventListener('click',()=>{
+const ecran = document.querySelector('.ecran')
+let operation  = ecran.value;
+const boutonUn = document.querySelector('.un')
+const boutonDeux = document.querySelector('.deux')
+const boutonTrois = document.querySelector('.trois')
+const boutonQuatre = document.querySelector('.quatre')
+const boutonCinq = document.querySelector('.cinq')
+const boutonSix = document.querySelector('.six')
+const boutonSept = document.querySelector('.sept')
+const boutonHuit = document.querySelector('.huit')
+const boutonNeuf = document.querySelector('.neuf')
+const boutonZero = document.querySelector('.zero')
+const boutonPlus = document.querySelector('.plus')
+const boutonMoins = document.querySelector('.moins')
+const boutonX = document.querySelector('.multiplie')
+const boutonDivision = document.querySelector('.divise')
+const boutonPoint = document.querySelector('.point')
+const boutonEffacer = document.querySelector('.effacer')
+const boutonEgal = document.querySelector('.egal')
 
-        changeAll()
+
+boutonUn.addEventListener('click', ()=>{
+
+        addToDisplay(boutonUn.innerHTML)
+
+} )
+
+boutonDeux.addEventListener('click', ()=>{
+
+    addToDisplay(boutonDeux.innerHTML)
+
+} )
+boutonTrois.addEventListener('click', ()=>{addToDisplay(boutonTrois.innerHTML)} )
+boutonQuatre.addEventListener('click', ()=>{addToDisplay(boutonQuatre.innerHTML)} )
+boutonCinq.addEventListener('click', ()=>{addToDisplay(boutonCinq.innerHTML)} )
+boutonSix.addEventListener('click', ()=>{addToDisplay(boutonSix.innerHTML)} )
+boutonSept.addEventListener('click', ()=>{addToDisplay(boutonSept.innerHTML)} )
+boutonHuit.addEventListener('click', ()=>{addToDisplay(boutonHuit.innerHTML)} )
+boutonNeuf.addEventListener('click', ()=>{addToDisplay(boutonNeuf.innerHTML)} )
+boutonZero.addEventListener('click', ()=>{addToDisplay(boutonZero.innerHTML)} )
+boutonPoint.addEventListener('click', ()=>{addToDisplay(boutonPoint.innerHTML)} )
+boutonPlus.addEventListener('click', ()=>{addToDisplay(boutonPlus.innerHTML)} )
+boutonMoins.addEventListener('click', ()=>{addToDisplay(boutonMoins.innerHTML)} )
+boutonX.addEventListener('click', ()=>{addToDisplay(boutonX.innerHTML)} )
+boutonDivision.addEventListener('click', ()=>{addToDisplay(boutonDivision.innerHTML)} )
+boutonEffacer.addEventListener('click' , ()=>{
+    effacer()
+})
+boutonEgal.addEventListener('click',()=>{
+    calcul()
 
 })
 
-function changeAll(){
-    //couleur au hasard
 
-    let hexChars = "abcdef0123456789"
-    let color = "#"
-    for(let i = 0; i<6;i++)
-        {
-            color += hexChars[Math.floor(Math.random()*hexChars.length)]
+// implementer les regles concernant les symboles
+
+function addToDisplay(valeurDuBouton)
+    {
+        let valeur = valeurDuBouton
+            //  operation = "1+2+5"
+
+      //  let lastChar = operation[operation.length-1]
+        let lastChar = operation.slice(-1)
+        let symbols = "+-/*."
+
+      let lastCharIsSymbol =  symbols.includes(lastChar)
+      let valeurIsSymbol =  symbols.includes(valeur)
+                                 //  AND
+        if(lastCharIsSymbol && valeurIsSymbol){
+                   return
         }
 
-    //hauteur, largeur et border radius au hasard
-    // h & w   =  30 à 300 px     br = 10% a 50%
+        if(operation == 0){
+            operation = valeur
+        }else{
+            operation = operation + valeur
+        }
 
-        let height = Math.random()*270 +30
-        let width = Math.random()*270 +30
-        let borderRadius = Math.random()*40 + 10
-        let top = Math.random()*80
-        let left = Math.random()*80
+        console.log("operation :" +operation)
 
-        cercle.style.backgroundColor = color
-        cercle.style.height = height + "px"
-        cercle.style.width = width + "px"
-        cercle.style.borderRadius = borderRadius + "%"
-        cercle.style.top = top + "%"
-        cercle.style.left = left + "%"
+        ecran.value = operation
+    }
 
-    setTimeout(()=>{
-        console.log("voila le message apres 2 secondes")
+    function effacer(){
+        ecran.value=0
+        operation = '0'
+    }
 
-    },2000 )
+    function calcul(){
+    ecran.value = eval(ecran.value)
+    operation = ecran.value
+    }
 
-    setTimeout(()=>{
-        console.log("voilaun autre message")
 
-    },Math.random()*3000 )
+let bolognaise = 'à la sauce bolognaise'
 
-    console.log(new Date().getTime())
+function faisDesPates(sauce){
 
+    console.log('voici un plat de pates ' + sauce )
+}
+
+// faisDesPates(bolognaise)
+
+
+
+
+//en soi, dans un code couleur,
+//il y a un #
+//et ensuite 6 caracteres parmis ceux-la
+
+
+for( let i = 0   ; i <= 4  ; i++   )
+{
+   // console.log("coucou")
+    console.log(i)
 
 }
+
+
 
